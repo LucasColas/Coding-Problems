d4a029f23d3fa6727325ce107f2e94d804570678
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2021-10-27 21:00:15 +0200

    Create RTI.cpp

---
+++ Roman To Integer/RTI.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) { //working in progress
+        int num = 0;
+        map<string,int> dic;
+        dic['I'] = 1;
+        dic['V'] = 5;
+        dic['X'] = 10;
+        dic['L'] = 50;
+        dic['C'] = 100;
+        dic['D'] = 500;
+        dic['M'] = 1000;
+        dic['IV'] = 4;
+        dic['IX'] = 9;
+        dic['XL'] = 40;
+        dic['XC'] = 90;
+        dic["CD"] = 400;
+        dic['CM'] = 900;
+        while (s.length() != 0) {
+            for ()
+        }
+    }
+};
