8774b41c8630c94f77158ec25fa98ecc99b3b1c6
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2021-11-11 06:59:46 +0100

    Update Add Binary.cpp

--- Add Binary/Add Binary.cpp
+++ Add Binary/Add Binary.cpp
@@ -7,6 +7,9 @@ public:
         int j = b.length();
         string ans = "";
         string qtity = "0";
+        int lbi = -1;
+        string lb = "";
+        
         
         while ((i >= 0 ) && (j >= 0)) {
             if (a[i] == "0" && b[j] == "0") {
@@ -24,8 +27,35 @@ public:
                     ans = "0" + ans;
                 }
             }
-            
+            i--;
+            j--;       
             
         }
+        
+        if (j >= 0) {
+            lbi = j;
+            lb = b.substr(0,j+1); //first index and then the length
+        }
+        else if (i >= 0) {
+            lbi = i;
+            lb = a.substr(0, i+1);
+        }
+        
+        while (lbi >= 0) {
+            if (qtity == "1" && lb[lbi] == "1") {
+                sum = "0" + sum;
+            } 
+            else if (qtity == "1" || lb[lbi] == "1") {
+                sum = "1" + sum;
+            }
+            
+            else {
+                sum = lb[lbi]+sum;
+            }
+            
+            lbi--;
+            
+        }
+        
     }
 };
