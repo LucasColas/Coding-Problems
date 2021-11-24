2a7bd06c5ef1d50d797aa6afda190e27cb806087
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2021-11-24 06:46:43 +0100

    Update main.py

--- Iterator for Combination/main.py
+++ Iterator for Combination/main.py
@@ -1,17 +1,34 @@
+class CombinationIterator:
+  def __init__(self, characters, combinationLength):
+    self.characters = characters
+    self.combinationLength = combinationLength
+    self.every_comb = self.generate()
 
+  def generate(self):
+    every_comb = []
+    for i in range(2**len(self.characters)):
+      string = ""
+      for j in range(len(self.characters)):
 
+        if ((1<<j) & i > 0):
+          string += self.characters[j]
+        
+        if len(string) == self.combinationLength and string not in every_comb:
+          every_comb.append(string)
+    
+    every_comb.sort()
+    return every_comb
 
-for i in range(2**len(n)):
-  cursub_msg = "current subset contains : "
-  cursub = ""
-  for j in range(len(n)):
-    print(1<<j)
-    if ((1<<j) & i > 0):
-      cursub += (n[j])
-      if len(cursub) == 2 and cursub not in every_comb:
-        #print(cursub)
-        every_comb.append(cursub)
+  def nextStr(self):
+    for id in range(len(self.every_comb)):
+      return self.every_comb.pop(id)              
 
-  #print(cursub_msg, cursub)
+  def hasNext(self):
+    if len(self.every_comb) >= 1:
+      return True
 
-#print(every_comb)
+    return False
+        
+
+itr = CombinationIterator("bvwz", 2)
+#print(itr.every_comb)
