86cef485db6c0f4abf2dd4aa126edd64533720e6
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2021-11-25 06:35:00 +0100

    Update IFC.cpp

--- Iterator for Combination/IFC.cpp
+++ Iterator for Combination/IFC.cpp
@@ -13,16 +13,37 @@ public:
     }
     
     vector generate() {
+        vector<string> comb;
         for (int i = 0; i < 2**characters.size(); i++) {
+            string str = "";
+            for (int j = 0; j < characters.size(); j++) {
+                if ((1<<j) & i > 0) {
+                    str += characters[j];
+                
+            }
+                
+            if str.size() == combinationLength {
+                comb.push_back(str);
+            }
             
         }
     }
     
     string next() {
+        for (int w = 0; w < every_comb.size(); w++) {
+            string new_str = every_comb[w];
+            every_comb.erase(w);
+            return new_str
+        }
         
     }
     
     bool hasNext() {
+        if next() {
+            return true;
+        }
+        
+        return false;
         
     }
 };
