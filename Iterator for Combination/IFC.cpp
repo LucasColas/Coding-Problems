9cd711773e49d0036b5e26d7a3beb5cbbb90d314
LucasColas <lucas.colas13@gmail.com>
2021-11-30 06:29:13 +0100

    update

--- Iterator for Combination/IFC.cpp
+++ Iterator for Combination/IFC.cpp
@@ -30,7 +30,7 @@ class CombinationIterator {
             for (int j = 0; j < characters_.size(); j++) {
                 //std::cout << ((1<<j) & i>0);
                 //std::cout << (i & (1<<j));
-                if (i & (1<<j) != 0) {
+                if ((i & (1<<j)) != 0) {
                     //std::cout << characters_[j];
                     str += characters_[j];
                 }
