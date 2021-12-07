ea046c81693dae55505946ddf2b44a57aff87a0b
LucasColas <lucas.colas13@gmail.com>
2021-12-07 06:43:03 +0100

    update

---
+++ Unique Paths/unique_paths.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+//#include <algorithm>
+
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+
+    }
+private:
+  int findPaths(int m, int n) {
+    int mat[m][n]; // create 2D array 
+  }
+
+};
