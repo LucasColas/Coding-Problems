598606bb92d07dfd3788ed4b1d57c9f2b8b05ef5
LucasColas <lucas.colas13@gmail.com>
2021-12-07 06:43:22 +0100

    update

--- Unique Paths/unique_paths.cpp
+++ Unique Paths/unique_paths.cpp
@@ -7,11 +7,11 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-
+      return findPaths(m, n)
     }
 private:
   int findPaths(int m, int n) {
-    int mat[m][n]; // create 2D array 
+    int mat[m][n]; // create 2D array
   }
 
 };
