4e2d59215015be00aeddb6f9966d5a8b6a3fd58d
LucasColas <lucas.colas13@gmail.com>
2021-12-08 06:41:30 +0100

    update

--- Unique Paths/unique_paths.cpp
+++ Unique Paths/unique_paths.cpp
@@ -7,28 +7,24 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-      std::cout << "Yeah, uniquePaths" << std::endl;
       return findPaths(m, n);
     }
 private:
   int findPaths(int m, int n) {
-    std::cout << "findPaths" << std::endl;
+
     int mat[m][n]; // create 2D array
     for (int i = 0; i < m; i++) {
-      std::cout << "inside for " << std::endl;
-      for (int j = 0; i < n; j++) {
-        //std::cout << "inside nested for " << std::endl;
+      for (int j = 0; j < n; j++) {
+
         if ((i == 0 ) || (j == 0)) {
-          //std::cout << "i = 0 or j = 0" << std::endl;
           mat[i][j] = 1;
         } else {
           std::cout << "else" << std::endl;
           mat[i][j] = mat[i-1][j] + mat[i][j-1];
         }
+
       }
-      std::cout << "Finished nested loop";
     }
-    std::cout << "I am going to return" << std::endl;
 
     return mat[m-1][n-1];
   }
@@ -40,7 +36,6 @@ private:
 int main() {
   Solution S;
   int Sol = S.uniquePaths(3,7);
-  std::cout << "Hello";
-  std::cout << Sol;
+  
   return 0;
 }
