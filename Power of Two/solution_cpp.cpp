a3e537dab4303130722e8c9a4a3d0abbd235f668
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2021-12-21 06:12:28 +0100

    Create solution_cpp.cpp

---
+++ Power of Two/solution_cpp.cpp
@@ -0,0 +1,18 @@
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+Source : https://leetcode.com/problems/power-of-two/
+*/
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        } 
+        //cout << n << endl;
+        return ((n & (n-1)) == 0);
+    }
+};
