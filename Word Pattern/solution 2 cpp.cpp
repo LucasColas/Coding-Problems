0f004b57b8715f0ca3b35c59f17fb10afdd1df0a
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-01-17 16:35:09 +0100

    Update solution 2 cpp.cpp

--- Word Pattern/solution 2 cpp.cpp
+++ Word Pattern/solution 2 cpp.cpp
@@ -1,4 +1,12 @@
+/*
 
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+From leetcode : https://leetcode.com/problems/word-pattern/
+
+*/
 
 #include <map>
 
