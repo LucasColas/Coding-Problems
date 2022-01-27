1ec5cdb443c8a987428a79e15580890f62bfd583
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-01-27 07:02:43 +0100

    Update solution C (brute force) .c

--- Maximum XOR of Two Numbers/solution C (brute force) .c
+++ Maximum XOR of Two Numbers/solution C (brute force) .c
@@ -1,9 +1,9 @@
 int findMaximumXOR(int* nums, int numsSize){
     int max = 0;
     //printf("%d", numsSize);
-    for(int i=0;i < numsSize;i++)  // outer loop  
+    for(int i=0;i < numsSize;i++) 
    {  
-       for(int j=0;j < numsSize;j++)  // inner loop  
+       for(int j=0;j < numsSize;j++)  
        {
            int result = nums[i] ^ nums[j];
            if (i != j &&  result >= max) {
