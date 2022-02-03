3174740aeaa890e3258c9c19c7695f07613a507a
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-02-03 10:35:27 +0100

    Update solution py.py

--- 4Sum II/solution py.py
+++ 4Sum II/solution py.py
@@ -1,8 +1,7 @@
 """
 Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
-
-    0 <= i, j, k, l < n
-    nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+-0 <= i, j, k, l < n
+-nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
 From Leetcode : https://leetcode.com/problems/4sum-ii/
 
 """
