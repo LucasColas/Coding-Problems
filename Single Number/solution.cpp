e02c1ebad84f46efb5fcf5ab5cd92c72bca128a9
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-02-15 08:53:50 +0100

    Update solution.cpp

--- Single Number/solution.cpp
+++ Single Number/solution.cpp
@@ -4,6 +4,8 @@ Given a non-empty array of integers nums, every element appears twice except for
 
 You must implement a solution with a linear runtime complexity and use only constant extra space.
 
+From Leetcode : https://leetcode.com/problems/single-number/
+
 */
 
 class Solution {
