cdb63b26f6c04cb148e2e3cb18288d8e8be8095c
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-05-15 13:46:40 +0200

    Create Deepest Leaves Sum.c

---
+++ Deepest Leaves Sum/Deepest Leaves Sum.c
@@ -0,0 +1,57 @@
+/*
+
+Given the root of a binary tree, return the sum of values of its deepest leaves. 
+From Leetcode : https://leetcode.com/problems/deepest-leaves-sum/
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int goDeepest(struct TreeNode* root, int h, int h_max) {
+    if (h == h_max) {
+        if (root != NULL) {
+            //printf("root Null : \n");
+            return root->val;
+        }
+        else {
+            return 0;
+            
+        } 
+    }
+    
+    if (root != NULL) {
+        //printf("goDeepest \n");
+        //printf("val : %d\n", root->val);
+        return goDeepest(root->left, h+1, h_max) + goDeepest(root->right, h+1, h_max);
+    }
+    
+    return 0;
+    
+    
+}
+
+int height(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    
+    int left = height(root->left);
+    int right = height(root->right);
+    return left > right ? left+1 : right+1;
+}
+
+int deepestLeavesSum(struct TreeNode* root){
+    int h_max = height(root);
+    //printf("%d", h_max);
+    return goDeepest(root, 1, h_max);
+    
+}
+
